21487e2c91e95d5d3c39c26e2df2d00adb436d5d
mmkhl <pmisha1014@gmail.com>
2022-09-06 23:53:36 +0600

    new line in main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# Три сопротивления R u R 2y R 3 соединены параллельно. Найти сопротивление соединения.
+
+firstResistance = int(input("Введите значение R1: "))
+secondResistance = int(input("Введите значение R2: "))
+thirdResistance = int(input("Введите значение R3: "))

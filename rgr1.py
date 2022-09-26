a0b871dddfcd6a99af073cc005971b68cf762670
mmkhl <pmisha1014@gmail.com>
2022-09-26 09:19:28 +0600

    new changes in code

--- rgr1.py
+++ rgr1.py
@@ -6,23 +6,23 @@ from tkinter import *
 from tkinter import messagebox
 
 someNum = randint(1, 42)
-myNum = 0
 flagGame = True
 sumAttempt = 0
 
 def startGame():
-    global sumAttempt
-    myNum = int(message.get())
-    if myNum > someNum:
-        messagebox.showinfo("Подсказка", "Ваше число больше загаданного")
-        sumAttempt += 1
-    elif myNum < someNum:
-        messagebox.showinfo("Подсказка", "Ваше число меньше загаданного")
-        sumAttempt += 1
-    elif message.get() == "":
+    if message.get() == '':
         messagebox.showinfo("Подсказка", "Вы не ввели число")
-    elif myNum == someNum:
-        messagebox.showinfo("Победа", "Вы угадали, это число: " + str(someNum) +"\n Количество попыток: " + str(sumAttempt))
+    else:
+        global sumAttempt
+        myNum = int(message.get())
+        if myNum > someNum:
+            messagebox.showinfo("Подсказка", "Ваше число больше загаданного")
+            sumAttempt += 1
+        elif myNum < someNum:
+            messagebox.showinfo("Подсказка", "Ваше число меньше загаданного")
+            sumAttempt += 1
+        elif myNum == someNum:
+            messagebox.showinfo("Победа", "Вы угадали, это число: " + str(someNum) +"\n Количество попыток: " + str(sumAttempt))
 
 def show_message():
     messagebox.showinfo("Подсказка", message.get())
@@ -33,14 +33,16 @@ root.title("Угадай число")
 root.geometry("300x200")
 
 message = StringVar()
+lableText = Label(text="Введите число", font=("Times New Roman", 15))
+lableText.place(relx=.5, rely=.2,  anchor="center")
 result_entry = Entry(textvariable=message)
-result_entry.place(relx=.5, rely=.2,  anchor="c")
+result_entry.place(relx=.5, rely=.5, width=140, height=30, anchor="center")
 
 programm_button = Button(text="Очистить", font=("Times New Roman", 15), command=clear)
-programm_button.place(relx=.3, rely=.5, anchor="c")
+programm_button.place(relx=.3, rely=.8, anchor="center")
 
 programm_button = Button(text="Проверить", font=("Times New Roman", 15), command=startGame)
-programm_button.place(relx=.7, rely=.5, anchor="c")
+programm_button.place(relx=.7, rely=.8, anchor="center")
 root.mainloop()
 
 

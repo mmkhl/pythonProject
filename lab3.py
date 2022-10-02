26080a1d3cbaf7e0fd1d913efe404a096a0ed61e
mmkhl <pmisha1014@gmail.com>
2022-10-02 17:24:26 +0600

    final rgr1

--- lab3.py
+++ lab3.py
@@ -2,45 +2,43 @@
 # Решение оформите в виде функции MinDivisor(n). Алгоритм должен иметь сложность O(n√).
 # Указание. Если у числа n нет делителя не превосходящего n−−√, то число n — простое и ответом будет само число n.
 #
-# num = int(input("Введите число, отличное от нуля: "))
-#
-#
-# def MinDivisor(n):
-#     if n <= 1:
-#         print("Введенное число должно быть больше единицы!")
-#     else:
-#         if n % n ** 0.5 == 0:
-#             return int(n ** 0.5)
-#         else:
-#             return n
-#
-#
-# print("Результат: ", MinDivisor(num))
+num = int(input("Введите число, отличное от нуля: "))
+
+def MinDivisor(n):
+    if n <= 1:
+        print("Введенное число должно быть больше единицы!")
+    else:
+        if n % n ** 0.5 == 0:
+            return int(n ** 0.5)
+        else:
+            return n
+
+print("Результат: ", MinDivisor(num))
 
 # Выведите значение наименьшего нечетного элемента списка, а если в списке нет нечетных элементов - выведите число 0.
 # Входные данные
 # Вводится список чисел. Все числа списка находятся на одной строке.
 
-# someNumList = list(map(int, input().split()))
-# defaultMin = 0
-# def checkMin(num):
-#     global defaultMin
-#     for i in range(len(num)):
-#         if num[i] % 2 == 1:
-#             defaultMin = num[i]
-#             break
-#     for i in range(len(num)):
-#         if num[i] % 2 == 1:
-#             if num[i] < defaultMin:
-#                 defaultMin = num[i]
-#     if defaultMin != 0:
-#         print(defaultMin)
-#     else:
-#         print(0)
-#     return defaultMin
-# print("Минимальное значение: ", checkMin(someNumList))
+someNumList = list(map(int, input().split()))
+defaultMin = 0
+def checkMin(num):
+    global defaultMin
+    for i in range(len(num)):
+        if num[i] % 2 == 1:
+            defaultMin = num[i]
+            break
+    for i in range(len(num)):
+        if num[i] % 2 == 1:
+            if num[i] < defaultMin:
+                defaultMin = num[i]
+    if defaultMin != 0:
+        print(defaultMin)
+    else:
+        print(0)
+    return defaultMin
+print("Минимальное значение: ", checkMin(someNumList))
 
 # A: Четные индексы
 # Решите эту задачу в одну строку
 
-# print(" ".join(input().split()[::2]))
+print(" ".join(input().split()[::2]))
--- rgr1.py
+++ rgr1.py
@@ -1,20 +1,27 @@
 # Необходимо написать любую игру на питоне
 # игра "Угадай число" представляет из себя программу с консольным вводом выводом
 
+import tkinter as tk
 from random import randint
-from tkinter import *
 from tkinter import messagebox
+import os
 
 someNum = randint(1, 42)
-flagGame = True
 sumAttempt = 0
 
-def startGame():
-    if message.get() == '':
+def game_Process():
+    # message = tk.StringVar()
+
+    def get_entry(result_entry):
+        s = result_entry.get()
+        return int(s)
+
+    myNum = get_entry(result_entry)
+    if myNum == '':
         messagebox.showinfo("Подсказка", "Вы не ввели число")
     else:
         global sumAttempt
-        myNum = int(message.get())
+        myNum = get_entry(result_entry)
         if myNum > someNum:
             messagebox.showinfo("Подсказка", "Ваше число больше загаданного")
             sumAttempt += 1
@@ -23,26 +30,48 @@ def startGame():
             sumAttempt += 1
         elif myNum == someNum:
             messagebox.showinfo("Победа", "Вы угадали, это число: " + str(someNum) +"\n Количество попыток: " + str(sumAttempt))
+            f = open('results.txt', 'a')
+            f.write(str(sumAttempt))
+            f.close()
 
-def show_message():
-    messagebox.showinfo("Подсказка", message.get())
-def clear():
-    result_entry.delete(0, END)
-root = Tk()
-root.title("Угадай число")
-root.geometry("300x200")
+def startGame():
+    message = tk.StringVar()
+    def clear():
+        result_entry = tk.Entry(textvariable=message)
+        result_entry.delete(0, tk.END)
 
-message = StringVar()
-lableText = Label(text="Введите число", font=("Times New Roman", 15))
-lableText.place(relx=.5, rely=.2,  anchor="center")
-result_entry = Entry(textvariable=message)
-result_entry.place(relx=.5, rely=.5, width=140, height=30, anchor="center")
 
-programm_button = Button(text="Очистить", font=("Times New Roman", 15), command=clear)
-programm_button.place(relx=.3, rely=.8, anchor="center")
 
-programm_button = Button(text="Проверить", font=("Times New Roman", 15), command=startGame)
-programm_button.place(relx=.7, rely=.8, anchor="center")
-root.mainloop()
+    newWindow = tk.Toplevel(app)
+    newWindow.title("Игра")
+    newWindow.geometry("400x300")
+    global result_entry
+
+    result_entry = tk.Entry(newWindow, textvariable=message)
+    result_entry.place(relx=.5, rely=.5 )
+    lableStart = tk.Label(newWindow,  text = "Угадай число")
+    buttonCheck = tk.Button(newWindow, text = "Проверить", command=game_Process)
+    buttonClear = tk.Button(newWindow, text="Очистить", command=clear)
+    lableStart.pack(expand=True)
+    result_entry.pack(expand=True)
+    buttonCheck.pack(expand=True)
+    buttonCheck.invoke()
+    buttonClear.pack(expand=True)
+
+
+app = tk.Tk()
+app.title("Игра УГАДАЙ ЧИСЛО")
+app.geometry("300x250")
+
+menu_lable = tk.Label(text="Угадай число")
+btnStart = tk.Button(app, text="Начать", command=startGame)
+btnEnd = tk.Button(app, text="Выйти", command=quit)
+menu_lable.pack(expand=True)
+
+btnStart.pack(expand=True)
+btnEnd.pack(expand=True)
+
+app.mainloop()
+
 
 
